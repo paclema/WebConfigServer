afa4c16a0c877bc3a23f53458133123e948e11c7
Pablo Clemente <paclema@gmail.com>
2021-08-05 17:19:44 +0200

    Added WebConfigOTA class

---
+++ src/WebConfigOTA.cpp
@@ -0,0 +1,38 @@
+#include "WebConfigOTA.h"
+
+
+void WebConfigOTA::init(void) {
+  // Port defaults to 8266
+  #ifdef ESP32
+  ArduinoOTA.setPort(3232);
+  #elif defined(ESP8266)
+  ArduinoOTA.setPort(8266);
+  #endif
+
+  // Hostname defaults to esp8266-[ChipID]
+  if (hostnameOTA != "") {
+    char hostname[sizeof(this->hostnameOTA)];
+    hostnameOTA.toCharArray(hostname, sizeof(hostname));
+    ArduinoOTA.setHostname(hostname);
+  }
+
+  ArduinoOTA.begin();
+  ArduinoOTA.onStart([]() {
+    SPIFFS.end();
+    Serial.println("\n OTA update started... ");
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    // Log.info("OTA Progress: %i%%", (progress / (total / 100)));
+    Serial.printf("OTA Progress: %i%%\n", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("OTA update finished!");
+  });
+
+};
+
+void WebConfigOTA::parseWebConfig(JsonObjectConst configObject){
+  this->enabled = configObject["enabled"] | false;
+};
---
+++ src/WebConfigOTA.h
@@ -0,0 +1,32 @@
+#ifndef WebConfigOTA_H
+#define WebConfigOTA_H
+
+#include <Arduino.h>
+
+// OTA Includes
+#include <ArduinoOTA.h>
+
+#include "IWebConfig.h"
+
+class WebConfigOTA: public IWebConfig{
+
+private:
+  bool enabled = false;
+  String hostnameOTA = "";
+
+
+public:
+
+  void init(void);
+  void handle(void) { ArduinoOTA.handle(); }
+
+  void setHostname(String hostname){ this->hostnameOTA = hostname;}
+
+  bool isEnabled(void) { return this->enabled; }
+
+  void parseWebConfig(JsonObjectConst configObject);
+
+
+};
+
+#endif
